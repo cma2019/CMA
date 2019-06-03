bf879dce7e8612097503658835d424964fe55181
zfy <934542721@qq.com>
2019-06-03 21:11:58 +0800

    collapse

--- client/CMA/pages/IntermediateCheck/IntermediateCheckMenu/IntermediateCheckMenu.js
+++ client/CMA/pages/IntermediateCheck/IntermediateCheckMenu/IntermediateCheckMenu.js
@@ -1,9 +1,14 @@
 // pages/IntermediateCheckMenu/IntermediateCheckMenu.js
 Page({
   data: {
-
+    activeNames: ['0']
   },
-
+  onChange(event) {
+    console.log(event)
+    this.setData({
+      activeNames: event.detail
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
