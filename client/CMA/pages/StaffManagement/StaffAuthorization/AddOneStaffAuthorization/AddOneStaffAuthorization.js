2ca67f3580cd6d96cf1d4890cfbda9bb1b04eaee
gitdhs <269718306@qq.com>
2019-07-02 09:21:27 +0800

    dhs

--- client/CMA/pages/StaffManagement/StaffAuthorization/AddOneStaffAuthorization/AddOneStaffAuthorization.js
+++ client/CMA/pages/StaffManagement/StaffAuthorization/AddOneStaffAuthorization/AddOneStaffAuthorization.js
@@ -1,66 +1,49 @@
-// pages/StaffManagement/StaffAuthorization/AddOneStaffAuthorization/AddOneStaffAuthorization.js
+const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  onShow: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  bindDateChange(e) {
+    this.setData({
+      authorizerDate: e.detail.value
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  ApplicationAdd: function (e) {
+    {
+      console.log('form发生了add事件，携带数据为：', e.detail.value)
+      let url = app.globalData.url + 'StaffAuthorization/addOne'
+      let data = {
+        "id": e.detail.value.id,
+        "authorizerId": e.detail.value.authorizerId,
+        "content": e.detail.value.content,
+        "authorizerDate": e.detail.value.authorizerDate
+      }
+      console.log(url)
+      console.log(data)
+      app.wxRequest(url, 'POST', data, (res) => {
+        console.log('successfully')
+        console.log(res)
+        console.log(res.msg)
+        console.log(res.code)
+      }, (err) => {
+        console.log('fail intermediate check register')
+      })
+
+    }
 
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  goback: function () {
+    wx.navigateBack({
+      delta: 1
+    })
   }
 })
--- client/CMA/pages/StaffManagement/StaffAuthorization/GetAllByStaff/GetAllByStaff.js
+++ client/CMA/pages/StaffManagement/StaffAuthorization/GetAllByStaff/GetAllByStaff.js
@@ -1,66 +1,52 @@
-// pages/StaffManagement/StaffAuthorization/GetAllByStaff/GetAllByStaff.js
+// pages/StaffManagement/StaffManagement.js
+const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    "mess": null
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    //console.log(this.data.planId)
+    this.setData({
+      id: options.id
+    })
+    console.log(this.data.id)
+    let url = app.globalData.url + 'StaffAuthorization/getAllByStaff'
+    let postdata = {
+      "id": this.data.id
+    }
+    console.log(url)
+    console.log(postdata)
+    app.wxRequest(url, 'GET', postdata, (res) => {
+      //console.log('success')
+      console.log(res)
+      console.log('success')
+      console.log(res.code)
+      //console.log(res.msg)
+      console.log(res.data)
+      //var temp = res.data
+      //this.temp = temp
+      this.setData({
+        mess: res.data
+      })
 
+      console.log(this.mess)
+    }, (err) => {
+      //console.err('getone error')
+      wx.showToast({
+        title: 'getallbystaff error',
+        duration: 1500
+      })
+      console.log('getone error')
+    })
+  },
+  onShow: function (options) {
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  goback: function () {
+    wx.navigateBack({
+      delta: 1
+    })
   }
 })
