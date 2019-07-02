b19fbe77ac062994ede0f6e10704e373ff618dd6
Wh-night <2822617014@qq.com>
2019-07-02 12:27:41 +0800

    1

---
+++ server/cma/src/test/java/com/example/demo/Controller/SampleIoControllerTest.java
@@ -0,0 +1,85 @@
+package com.example.demo.Controller;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+@Transactional
+public class SampleIoControllerTest {
+    @Autowired
+    SampleIoController sampleIoController;
+    MockMvc mockMvc;
+
+    public SampleIoControllerTest() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new Object[]{this.sampleIoController}).build();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addOne() throws Exception {
+        String url = "/cma/SampleIo/addOne?sampleNumber=2019001&sampleName=天猫超市&sampleAmount=1&sampleState=0&sender=zhangsan&receiver=lisi&sendDate=2019-6-10&obtainer=王五&obtainDate=2019-7-1";
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post(url, new Object[0])).andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        String res = mvcResult.getResponse().getContentAsString();
+        int code = Integer.parseInt(res.substring(19, 22));
+        Assert.assertEquals(200L, (long)code);
+    }
+
+    @Test
+    public void deleteOne() throws Exception {
+        String url = "/cma/SampleIo/deleteOne?sampleIoId=4";
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post(url, new Object[0])).andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        String res = mvcResult.getResponse().getContentAsString();
+        int code = Integer.parseInt(res.substring(19, 22));
+        Assert.assertEquals(200L, (long)code);
+    }
+
+    @Test
+    public void findALL() throws Exception {
+        String url = "/cma/SampleIo/getAll";
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get(url, new Object[0])).andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        String res = mvcResult.getResponse().getContentAsString();
+        int code = Integer.parseInt(res.substring(19, 22));
+        Assert.assertEquals(200L, (long)code);
+    }
+
+    @Test
+    public void findOne() throws Exception {
+        String url = "/cma/SampleIo/getOne?sampleIoId=4";
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get(url, new Object[0])).andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        String res = mvcResult.getResponse().getContentAsString();
+        int code = Integer.parseInt(res.substring(19, 22));
+        Assert.assertEquals(200L, (long)code);
+    }
+
+    @Test
+    public void modify() throws Exception {
+        String url = "/cma/SampleIo/modifyOne?sampleIoId=8&sampleNumber=2019003&sampleName=天猫超市&sampleAmount=1&sampleState=0&sender=zhangsan&receiver=lisi&sendDate=2019-6-10&obtainer=王五&obtainDate=2019-7-1";
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.post(url, new Object[0])).andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print()).andReturn();
+        String res = mvcResult.getResponse().getContentAsString();
+        int code = Integer.parseInt(res.substring(19, 22));
+        Assert.assertEquals(200L, (long)code);
+    }
+}
