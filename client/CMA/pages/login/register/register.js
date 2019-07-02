0ea698b6c21c6a128d9375404fc26416f290736e
gitdhs <269718306@qq.com>
2019-07-02 15:20:18 +0800

    dhs

--- client/CMA/pages/login/register/register.js
+++ client/CMA/pages/login/register/register.js
@@ -1,66 +1,45 @@
-// pages/login/register/register.js
+const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  onShow: function (options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  ApplicationAdd: function (e) {
+    {
+      console.log('form发生了add事件，携带数据为：', e.detail.value)
+      let url = app.globalData.url + 'user/add'
+      let data = {
+        "user": e.detail.value.user,
+        "password": e.detail.value.password
+      }
+      console.log(url)
+      console.log(data)
+      app.wxRequest(url, 'POST', data, (res) => {
+        console.log('successfully')
+        console.log(res)
+        console.log(res.msg)
+        console.log(res.code)
+        wx.redirectTo({
+          url: '/pages/login/login/login',
+        })
+      }, (err) => {
+        console.log('fail intermediate check register')
+      })
+
+    }
 
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  goback: function () {
+    wx.navigateBack({
+      delta: 1
+    })
   }
 })
