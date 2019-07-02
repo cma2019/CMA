41ab2b51ce58cde3bd42ce14ac3ff08a6db9ea18
hzhahu <hzh.ahu@qq.com>
2019-07-02 09:57:50 +0800

    hzh

--- server/cma/src/main/java/com/example/demo/Controller/StaffLeavingController.java
+++ server/cma/src/main/java/com/example/demo/Controller/StaffLeavingController.java
@@ -38,6 +38,12 @@ public class StaffLeavingController {
     @PostMapping(path = "addOne")
     public @ResponseBody JSONObject addOne(@RequestParam(required = false,value = "id")long id,@RequestParam(required = false,value = "LeavingDate")String LeavingDate){
         JSONObject json=new JSONObject();
+        if(staffManagementRepository.existsById(id)==false){
+            json.put("code",210);
+            json.put("msg","不存在员工");
+            json.put("data",null);
+            return null;
+        }
         StaffManagement staff=staffManagementRepository.getOne(id);
         StaffLeaving staffLeaving=new StaffLeaving();
         staffLeaving.setStaffid(staff.getId());
