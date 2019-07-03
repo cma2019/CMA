5cae4349aefae4573418166de6360a78d975d3b6
jammonkeyyy <1538852806@qq.com>
2019-07-03 17:33:14 +0800

    文件处理demo及依赖

---
+++ server/cma/src/main/java/com/example/demo/Controller/FileController.java
@@ -0,0 +1,177 @@
+package com.example.demo.Controller;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import com.alibaba.fastjson.JSON;
+//import com.example.demo.FileControl.FileUploadConfig;
+import com.example.demo.framework.Response;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("cma/file")
+public class FileController {
+    /**
+     * 单文件上传
+     *
+     * @param file
+     * @param request
+     * @return response
+     * 本地路径为String path="F:/img/upload/"（统一）
+     */
+    @PostMapping("/upload")
+    @ResponseBody
+    public Response upload(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+        Response response=new Response();
+        if (!file.isEmpty()) {
+            String saveFileName = file.getOriginalFilename();
+            String path="F:/img/upload/";
+            File saveFile = new File(path + saveFileName);
+            File saveFilel = new File(path );
+            if (!saveFile.getParentFile().exists()) {
+                saveFile.getParentFile().mkdirs();
+            }
+            System.out.println(ServletFileUpload.isMultipartContent(request));
+            try {
+                DiskFileItemFactory factory=new DiskFileItemFactory();
+                factory.setSizeThreshold(4096*1000);
+                factory.setRepository(saveFilel);
+                ServletFileUpload upload=new ServletFileUpload(factory);
+                upload.setSizeMax(4194304*3);
+                List<FileItem>items=upload.parseRequest(request);
+                System.out.println(items);
+                for(FileItem item:items)
+                {
+                    System.out.println("1111111111");
+                    System.out.println(item.getFieldName());
+                    System.out.println(item.getName());
+                    System.out.println("2222222222");
+                }
+                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(saveFile));
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+                response.code=200;
+                response.msg=saveFile.getName()+"上传成功";
+                response.data=null;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                response.code=500;
+                response.msg="上传失败";
+            } catch (IOException e) {
+                e.printStackTrace();
+                response.code=500;
+                response.msg="上传失败";
+            }catch (FileUploadException e) {
+                e.printStackTrace();
+                response.code=500;
+                response.msg="上传失败";
+            }
+        } else {
+            response.code=500;
+            response.msg="上传失败,文件为空";
+        }
+        return response;
+    }
+
+    /**
+     * 多文件上传
+     *
+     * @param request
+     * @return
+     */
+ /*  @PostMapping("/uploadFiles")
+    @ResponseBody
+    public String uploadFiles(HttpServletRequest request) throws IOException {
+        File savePath = new File(request.getSession().getServletContext().getRealPath("/upload/"));
+        if (!savePath.exists()) {
+            savePath.mkdirs();
+        }
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        MultipartFile file = null;
+        BufferedOutputStream stream = null;
+        for (int i = 0; i < files.size(); ++i) {
+            file = files.get(i);
+            if (!file.isEmpty()) {
+                try {
+                    byte[] bytes = file.getBytes();
+                    File saveFile = new File(savePath, file.getOriginalFilename());
+                    stream = new BufferedOutputStream(new FileOutputStream(saveFile));
+                    stream.write(bytes);
+                    stream.close();
+                } catch (Exception e) {
+                    if (stream != null) {
+                        stream.close();
+                        stream = null;
+                    }
+                    return "第 " + i + " 个文件上传有错误" + e.getMessage();
+                }
+            } else {
+                return "第 " + i + " 个文件为空";
+            }
+        }
+        return "所有文件上传成功";
+    }*/
+
+    @RequestMapping("/download")
+    public String downloadFile(HttpServletRequest request, HttpServletResponse response) {
+        String fileName="test.jpg";
+       // Response myresponse=new Response();
+        if (fileName != null) {
+            //设置文件路径
+            String realPath = "F:/img/upload/";
+            File file = new File(realPath , fileName);
+            if (file.exists()) {
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                  //  myresponse.code=200;
+                 //   myresponse.msg="成功";
+                  //  myresponse.data=null;
+
+                } catch (Exception e) {
+                  //  myresponse.code=500;
+                  //  myresponse.msg="失败";
+                 //   myresponse.data=null;
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+}
