b4f653fce4a01f21e4bde93fd9c361315359b7ca
jammonkeyyy <1538852806@qq.com>
2019-07-03 17:33:44 +0800

    yxp

---
+++ server/cma/src/main/java/com/example/demo/Repository/TestingInstitutionInformationRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.Repository;
+import com.example.demo.Model.TestingInstitutionInformation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TestingInstitutionInformationRepository extends JpaRepository<TestingInstitutionInformation, Long>{
+}
