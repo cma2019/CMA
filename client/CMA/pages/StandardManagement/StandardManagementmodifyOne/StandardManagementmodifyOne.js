8207e518c57982b924badbaebb9961fa06ec6b0b
zfy <934542721@qq.com>
2019-07-04 18:38:20 +0800

    standardmanagement

--- client/CMA/pages/StandardManagement/StandardManagementmodifyOne/StandardManagementmodifyOne.js
+++ client/CMA/pages/StandardManagement/StandardManagementmodifyOne/StandardManagementmodifyOne.js
@@ -1,11 +1,46 @@
-// pages/StandardManagement/StandardManagementmodifyOne/StandardManagementmodifyOne.js
+// pages/TestingInstitutionManagement/Certificate/CertificateAddOne/CertificateAddOne.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    fileId: "test fileId",
+    fileName: "test.docx"
+  },
 
+  newEquipment: function (e) {
+    console.log('begin add')
+    var myurl = app.globalData.url + 'StandardManagement/modifyOne';
+    var mypath;
+    wx.chooseMessageFile({
+      count: 1,
+      type: 'all',
+      success: function (res) {
+        console.log("get file success")
+        console.log(res)
+        mypath = res.tempFiles[0].path
+        app.wxUploadFile(myurl, mypath, null, (res) => {
+          console.log("upload file success")
+          console.log(res)
+          wx.navigateBack({
+            delta: 1
+          })
+        }, (err) => {
+          console.log(err)
+        })
+        /*
+        wx.redirectTo({
+          url: '/pages/StandardManagement/StandardManagement',
+        })
+        */
+      },
+      fail: function (err) {
+        console.log("get file failed")
+        console.log(err)
+      }
+    })
   },
 
   /**
