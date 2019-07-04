1e85ee43379a85c84f12c6c0b1bc3c1e77dc7044
gitdhs <269718306@qq.com>
2019-07-04 15:27:59 +0800

    dhs

--- client/CMA/pages/ManagementReview/GetAllFileManagementReview/GetAllFileManagementReview.js
+++ client/CMA/pages/ManagementReview/GetAllFileManagementReview/GetAllFileManagementReview.js
@@ -1,66 +1,79 @@
-// pages/ManagementReview/GetAllFileManagementReview/GetAllFileManagementReview.js
+// pages/StaffManagement/StaffManagement.js
+const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    mess: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    this.setData({
 
+      year: options.id
+    })
+    
+    let url = app.globalData.url + 'ManagementReview/getAllFile'
+    let postdata = {
+      "year": this.data.year
+    }
+    console.log(url)
+    console.log(postdata)
+    app.wxRequest(url, 'GET', postdata, (res) => {
+      this.setData({
+        mess: res.data
+      })
+
+      console.log(this.data.mess)
+    }, (err) => {
+      //console.err('getone error')
+      wx.showToast({
+        title: 'getone error',
+        duration: 1500
+      })
+      console.log('getone error')
+    })
+  },
+  onShow: function (options) {
+    this.setData({
+
+      year: options.id
+    })
+
+    let url = app.globalData.url + 'ManagementReview/getAllFile'
+    let postdata = {
+      "year": this.data.year
+    }
+    console.log(url)
+    console.log(postdata)
+    app.wxRequest(url, 'GET', postdata, (res) => {
+      this.setData({
+        mess: res.data
+      })
+
+      console.log(this.data.mess)
+    }, (err) => {
+      //console.err('getone error')
+      wx.showToast({
+        title: 'getone error',
+        duration: 1500
+      })
+      console.log('getone error')
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  gotoAdd(e) {
+    wx.navigateTo({
+      url: 'AddOneManagementReview/AddOneManagementReview',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  gotoOne(e) {
+    console.log(e)
+    let target = e.currentTarget.id
+    console.log('getone id')
+    console.log(target)
+    wx.navigateTo({
+      url: 'GetAllFileManagementReview/GetAllFileManagementReview?id=' + target
+    })
   }
 })
