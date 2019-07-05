37a37e177ee138fc1dc7e9b2b70ad3a6ef9c21d3
gitdhs <269718306@qq.com>
2019-07-05 10:56:33 +0800

    dhs

--- client/CMA/pages/StaffManagement/StaffTraining/GetTrainingPeople/GetTrainingPeople.js
+++ client/CMA/pages/StaffManagement/StaffTraining/GetTrainingPeople/GetTrainingPeople.js
@@ -1,66 +1,54 @@
-// pages/StaffManagement/StaffTraining/GetTrainingPeople/GetTrainingPeople.js
+// pages/StaffManagement/StaffManagement.js
+const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    mess:[]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    //console.log(this.data.planId)
+    this.setData({
+      id: options.id
+    })
+    //console.log(this.data.year)
+    let url = app.globalData.url + 'StaffTraining/getTrainingPeople'
+    let postdata = {
+      "trainingId": this.data.id
+    }
+    console.log(url)
+    console.log(postdata)
+    app.wxRequest(url, 'GET', postdata, (res) => {
+      this.setData({
+        mess: res.data
+      })
 
+      console.log(this.data.mess)
+    }, (err) => {
+      //console.err('getone error')
+      wx.showToast({
+        title: 'getone error',
+        duration: 1500
+      })
+      console.log('getone error')
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  gotoAdd(e) {
+    wx.navigateTo({
+      url: '../AddTrainingPeople/AddTrainingPeople?id='+this.data.id,
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  gotoOne(e) {
+    console.log(e)
+    let target = [e.currentTarget.id,this.data.id]
+    console.log('getone id')
+    console.log(target)
+    wx.navigateTo({
+      url: 'PrintOneAnnualTrainingPlan/PrintOneAnnualTrainingPlan?id=' + target
+    })
   }
 })
