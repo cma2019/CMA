be70bdd130b6e6dba999f142b4d82502f15459cb
gitdhs <269718306@qq.com>
2019-07-05 16:38:21 +0800

    dhs

--- client/CMA/pages/StaffManagement/StaffFile/PrintOneStaffFile/PrintOneStaffFile.js
+++ client/CMA/pages/StaffManagement/StaffFile/PrintOneStaffFile/PrintOneStaffFile.js
@@ -1,66 +1,144 @@
-// pages/StaffManagement/StaffFile/PrintOneStaffFile/PrintOneStaffFile.js
+// pages/StaffManagement/PrintOneStaff/PrintOneStaff.js
+const app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    console.log(options)
+    this.setData({
+
+      id: options.id
+    })
+    console.log('getone id')
+    console.log(this.data.id)
+    let url = app.globalData.url + 'ManagementReview/getOne'
+    let postdata = {
+      "id": this.data.id
+    }
+    console.log(url)
+    console.log(postdata)
+    app.wxRequest(url, 'GET', postdata, (res) => {
+      console.log(res)
+      console.log(res.data)
+      //console.log(res.data[0].id)
+      if(res.code==210)
+      {
+        wx.showToast({
+          title: '档案不存在!',
+          icon:'none',
+          duration:2000
+        })
+      }
+      this.setData({
+        id: res.data.id,
+        name: res.data.name,
+        fileId: res.data.fileId,
+        fileLocation: res.data.fileLocation,
+        fileImage: res.data.fileImage
+      })
+    }, (err) => {
+      console.err('get one error')
+    })
+  },
+  ModifyStaff(e) {
+    console.log(e)
+    //let target = this.data.id
+    //console.log(target)
+    let url1 = app.globalData.url + 'ManagementReview/modifyOneFile'
+    let postdata1 = {
+      "fileId": this.data.id
+    }
+    console.log(postdata1)
+    app.wxRequest(url1, 'POST', postdata1, (res) => {
+      console.log(res)
+      console.log(res.data)
+      //console.log(res.data[0].id)
+    }, (err) => {
+      console.err('get one error')
+    })
+    var myurl2 = app.globalData.url + 'ManagementReview/addOneFile';
+    wx.chooseMessageFile({
+      count: 1,
+      type: 'all',
+      success: function (res) {
+        console.log("get file success")
+        console.log(res)
+        console.log(res.tempFiles)
+        console.log(res.tempFiles[0])
+        console.log(res.tempFiles[0].path)
+        //mypath = res.tempFiles[0].path
+        app.wxUploadFile(myurl2, res.tempFiles[0].path, null, (res) => {
+          console.log("upload file success")
+          console.log(res)
+          wx.showToast({
+            title: '修改成功!',
+            icon: 'success',
+            duration: 2000
+          })
+          //console.log(mydata)
+
+        }, (err) => {
+          console.log(err)
+        })
+        /*console.log(this.data.year)
+        console.log(this.year)
+        wx.redirectTo({
+          url: '../GetAllFileManagementReview/GetAllFileManagementReview?id=' + this.data.year,
+        })*/
+      },
+      fail: function (err) {
+        console.log("get file failed")
+        console.log(err)
+      }
+    })
+
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  DownloadStaff(e) {
+    //var that = this
+    var myurl = app.globalData.url + 'ManagementReview/downloadFile/' + this.data.id;
+    var myFilePath
+    app.wxDownloadFile(myurl, (res) => {
+      console.log("download one now")
+      console.log(res)
+      wx.saveFile({
+        tempFilePath: res.tempFilePath,
+        success: function (res) {
+          console.log("download now")
+          console.log(res)
+          myFilePath = res.savedFilePath
+          console.log(myFilePath)
+          wx.showToast({
+            title: '下载成功!',
+            icon: 'success',
+            duration: 2000
+          })
+        },
+        fail: function (err) {
+          console.log(err)
+        }
+      })
+    }, (err) => {
+      console.log(err)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  DeleteStaff(e) {
+    let url2 = app.globalData.url + 'ManagementReview/deleteOneFile'
+    let data2 = {
+      "fileId": this.data.id
+    }
+    app.wxRequest(url2, 'POST', data2, (res) => {
+      console.log('delete successfully')
+      wx.showToast({
+        title: '删除成功!',
+        icon: 'success',
+        duration: 2000
+      })
+    }, (err) => {
+      console.log('delete failed')
+    })
   }
+
 })
