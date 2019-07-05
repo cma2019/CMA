f3a38621656d1d44ee0b8591a2618300fa0279dc
njucszxy <1027575703@qq.com>
2019-07-05 10:20:24 +0800

    新增功能

--- client/CMA/pages/ExternalReviewManagement/ExternalReviewManagement/ExternalReviewManagement.js
+++ client/CMA/pages/ExternalReviewManagement/ExternalReviewManagement/ExternalReviewManagement.js
@@ -1,18 +1,43 @@
 // pages/ExternalReviewManagement/ExternalReviewManagement/ExternalReviewManagement.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    array: [{
+      "id": 1,
+      "year": 2019,
+      "date": "2019-7-5",
+    },
+    {
+      "id": 2,
+      "year": 2020,
+      "date": "2019-7-5",
+    }]
+  },
+  gotoAdd: function (e) {
+    wx.redirectTo({
+      url: '/pages/ExternalReviewManagement/ExternalReviewManagement/ExternalReviewManagementAddOne/ExternalReviewManagementAddOne',
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    var myurl = app.globalData.url + 'ExternalReviewManagement/getAll';
+    app.wxRequest(myurl, 'GET', null, (res) => {
+      console.log(res)
+      that.setData({
+        array: res.data.data
+      })
+      console.log(that.data.array)
+    }, (err) => {
+      console.log(err)
+    })
   },
 
   /**
@@ -26,7 +51,17 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this
+    var myurl = app.globalData.url + 'ExternalReviewManagement/getAll';
+    app.wxRequest(myurl, 'GET', null, (res) => {
+      console.log(res)
+      that.setData({
+        array: res.data.data
+      })
+      console.log(that.data.array)
+    }, (err) => {
+      console.log(err)
+    })
   },
 
   /**
