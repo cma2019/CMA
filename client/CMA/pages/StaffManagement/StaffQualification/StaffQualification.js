91f6039b1afcb91a4d2af01163c5562be6279ea3
gitdhs <269718306@qq.com>
2019-07-05 18:25:51 +0800

    dhs

--- client/CMA/pages/StaffManagement/StaffQualification/StaffQualification.js
+++ client/CMA/pages/StaffManagement/StaffQualification/StaffQualification.js
@@ -1,66 +1,63 @@
-// pages/StaffManagement/StaffQualification/StaffQualification.js
+// pages/StaffManagement/StaffManagement.js
+const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    mess: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
+    let url = app.globalData.url + 'StaffQualification/getAll'
+    let postdata = ''
+    console.log(url)
+    console.log(postdata)
+    app.wxRequest(url, 'GET', postdata, (res) => {
+      this.setData({
+        mess: res.data
+      })
 
+      console.log(this.data.mess)
+    }, (err) => {
+      //console.err('getone error')
+      wx.showToast({
+        title: '失败!',
+        duration: 1500
+      })
+      console.log('getone error')
+    })
+  },
+  onShow: function (options) {
+    let url1 = app.globalData.url + 'StaffQualification/getAll'
+    let postdata1 = ''
+    app.wxRequest(url1, 'GET', postdata1, (res) => {
+      this.setData({
+        mess: res.data
+      })
+
+      console.log(this.data.mess)
+    }, (err) => {
+      //console.err('getone error')
+      wx.showToast({
+        title: '失败!',
+        duration: 1500
+      })
+      console.log('getone error')
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  gotoAdd(e) {
+    wx.navigateTo({
+      url: 'AddOneStaffQualification/AddOneStaffQualification',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  gotoOne(e) {
+    console.log(e)
+    let target = e.currentTarget.id
+    console.log('getone id')
+    console.log(target)
+    wx.navigateTo({
+      url: 'PrintOneStaffQualification/PrintOneStaffQualification?id=' + target
+    })
   }
 })
