1214abc1e9b96a4919440fc853e603e915816a3d
gitdhs <269718306@qq.com>
2019-07-06 18:07:08 +0800

    dhs

--- client/CMA/pages/StaffManagement/StaffAuthorization/GetOneStaffAuthorization/GetOneStaffAuthorization.js
+++ client/CMA/pages/StaffManagement/StaffAuthorization/GetOneStaffAuthorization/GetOneStaffAuthorization.js
@@ -9,6 +9,30 @@ Page({
     this.setData({
       id: options.id
     })
+    console.log('getone id')
+    console.log(this.data.id)
+    let url = app.globalData.url + 'StaffAuthorization/getOne'
+    let postdata = {
+      "authorizationId": this.data.id
+    }
+    console.log(url)
+    console.log(postdata)
+    app.wxRequest(url, 'GET', postdata, (res) => {
+      console.log(res)
+      console.log(res.data)
+      //console.log(res.data[0].id)
+      this.setData({
+        name: res.data.name,
+        department: res.data.department,
+        position: res.data.position,
+        authorizerId: res.data.authorizerId,
+        content: res.data.content,
+        authorizerName: res.data.authorizerName,
+        authorizerDate: res.data.authorizerDate
+      })
+    }, (err) => {
+      console.err('get one error')
+    })
     
   },
   onShow:function()
@@ -42,7 +66,7 @@ Page({
     console.log(e)
     let target = this.data.id
     console.log(target)
-    wx.redirectTo({
+    wx.navigateTo({
       url: '../ModifyOneStaffAuthorization/ModifyOneStaffAuthorization?id=' + target,
     })
   },
