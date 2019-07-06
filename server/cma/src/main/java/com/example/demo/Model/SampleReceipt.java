1102538b0fdae9e29b8a154a63db03f28c6d9c3b
Wh-night <2822617014@qq.com>
2019-07-06 14:13:00 +0800

    LB

--- server/cma/src/main/java/com/example/demo/Model/SampleReceipt.java
+++ server/cma/src/main/java/com/example/demo/Model/SampleReceipt.java
@@ -1,8 +1,8 @@
 package com.example.demo.Model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.sql.Date;
 
