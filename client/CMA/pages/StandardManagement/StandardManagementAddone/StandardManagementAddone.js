b7a394453fd8bbb21eeaf3f144e441aebf2e3436
zfy <934542721@qq.com>
2019-07-06 18:02:21 +0800

    18.02.7.6

--- client/CMA/pages/StandardManagement/StandardManagementAddone/StandardManagementAddone.js
+++ client/CMA/pages/StandardManagement/StandardManagementAddone/StandardManagementAddone.js
@@ -24,21 +24,43 @@ Page({
         app.wxUploadFile(myurl, mypath, null, (res) => {
           console.log("upload file success")
           console.log(res)
-          wx.navigateBack({
-            delta: 1
-          })
+          if(res.code == 200){
+            wx.showToast({
+              title: '添加成功',
+              image: '/icons/ok/ok.png',
+              duration: 1000,
+              success: function () {
+                setTimeout(function () {
+                  wx.navigateBack({
+                    delta: 1
+                  })
+                }, 1000);
+              }
+            })
+          }else{
+            wx.showToast({
+              title: '添加失败',
+              image: '/icons/warning/warning.png',
+              duration: 1000
+            })
+          }
         }, (err) => {
           console.log(err)
+          wx.showToast({
+            title: '添加失败',
+            image: '/icons/warning/warning.png',
+            duration: 1000
+          })
         })
-        /*
-        wx.redirectTo({
-          url: '/pages/StandardManagement/StandardManagement',
-        })
-        */
       },
       fail: function (err) {
         console.log("get file failed")
         console.log(err)
+        wx.showToast({
+          title: '添加失败',
+          image: '/icons/warning/warning.png',
+          duration: 1000
+        })
       }
     })
   },
