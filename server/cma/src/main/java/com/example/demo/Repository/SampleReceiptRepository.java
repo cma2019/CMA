5de7365e3a99690d943bb37a3f637703d3b56c71
Wh-night <2822617014@qq.com>
2019-07-06 14:16:03 +0800

    LB

--- server/cma/src/main/java/com/example/demo/Repository/SampleReceiptRepository.java
+++ server/cma/src/main/java/com/example/demo/Repository/SampleReceiptRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.Repository;
 import com.example.demo.Model.SampleReceipt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+
 public interface SampleReceiptRepository extends JpaRepository<SampleReceipt, Long> {
     SampleReceipt findBySampleId(Long sampleid);
     List<SampleReceipt> findAll();
--- server/cma/src/main/java/com/example/demo/Repository/SampleReceiveRepository.java
+++ server/cma/src/main/java/com/example/demo/Repository/SampleReceiveRepository.java
@@ -1,7 +1,7 @@
 
 package com.example.demo.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
+//import org.springframework.data.jpa.repository.Modifying;
 import com.example.demo.Model.SampleReceive;
 import java.util.List;
 
