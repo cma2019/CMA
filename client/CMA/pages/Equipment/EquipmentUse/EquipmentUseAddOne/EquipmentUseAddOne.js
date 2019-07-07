6db63c64f49257bfa64f2e1dd5d4d50a638bdfd7
njucszxy <1027575703@qq.com>
2019-07-07 17:09:19 +0800

    添加注释

--- client/CMA/pages/Equipment/EquipmentUse/EquipmentUseAddOne/EquipmentUseAddOne.js
+++ client/CMA/pages/Equipment/EquipmentUse/EquipmentUseAddOne/EquipmentUseAddOne.js
@@ -1,10 +1,12 @@
 // pages/Equipment/EquipmentUse/EquipmentUseAddOne/EquipmentUseAddOne.js
+//获取全局app实例
 const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
+  //测试用数据
   data: {
     "id": null,
     "equipmentId": null,
@@ -18,35 +20,46 @@ Page({
     "user": null,
     "remark": null
   },
+  //返回按钮的处理函数
   mygo: function (e) {
+    //跳转回查找页面
     wx.redirectTo({
       url: '/pages/Equipment/EquipmentUse/EquipmentUse',
     })
   },
+  //时间控件的处理函数
   bindDateChange: function (e) {
     console.log("date")
     console.log(e.detail.value)
+    //动态设置时间变量的值
     this.setData({
       'useDate': e.detail.value
     })
   },
+  //时间控件的处理函数
   bindDateChange2: function (e) {
     console.log("date2")
     console.log(e.detail.value)
+    //动态设置时间变量的值
     this.setData({
       'openDate': e.detail.value
     })
   },
+  //时间控件的处理函数
   bindDateChange3: function (e) {
     console.log("date3")
     console.log(e.detail.value)
+    //动态设置时间变量的值
     this.setData({
       'closeDate': e.detail.value
     })
   },
+  //添加按钮的处理函数
   newEquipment: function (e) {
     console.log(e.detail.value)
+    //构造url
     var myurl = app.globalData.url + 'EquipmentUse/add';
+    //构造参数
     var mydata = {
       "equipmentId": e.detail.value.equipmentId,
       "useDate": e.detail.value.useDate,
@@ -59,15 +72,20 @@ Page({
       "user": e.detail.value.user,
       "remark": e.detail.value.remark
     };
+    //请求后端
     app.wxRequest(myurl, 'POST', mydata, (res) => {
+      //成功处理函数
       console.log("add")
       console.log(res)
+      //成功提示
       wx.showToast({
         title: '添加成功',
         icon: '/icons/ok/ok.png',
         duration: 1000,
         success: function () {
+          //延时
           setTimeout(function () {
+            //跳转回查找界面
             wx.redirectTo({
               url: '/pages/Equipment/EquipmentUse/EquipmentUse',
             })
@@ -75,6 +93,7 @@ Page({
         }
       })
     }, (err) => {
+      //失败处理函数
       console.log(err)
     })
   },
--- client/CMA/pages/ExternalReviewManagement/ExternalReviewManagement/ExternalReviewManagementYear/ExternalReviewManagementYearAddOne/ExternalReviewManagementYearAddOne.js
+++ client/CMA/pages/ExternalReviewManagement/ExternalReviewManagement/ExternalReviewManagementYear/ExternalReviewManagementYearAddOne/ExternalReviewManagementYearAddOne.js
@@ -1,10 +1,12 @@
 // pages/ExternalReviewManagement/ExternalReviewManagement/ExternalReviewManagementYear/ExternalReviewManagementYearAddOne/ExternalReviewManagementYearAddOne.js
+//获取全局app实例
 const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
+  //测试数据
   data: {
     "id": null,
     "year": null,
@@ -12,35 +14,50 @@ Page({
     "fileName": null,
     year:null
   },
+  //返回按钮的处理函数
   mygo: function (e) {
+    //保存指针
     var that = this
+    //跳转回查找页面
     wx.redirectTo({
       url: '/pages/ExternalReviewManagement/ExternalReviewManagement/ExternalReviewManagementYear/ExternalReviewManagementYear?year=' + that.data.year,
     })
   },
+  //添加按钮的处理函数
   newEquipment: function (e) {
     console.log(e.detail.value)
+    //保存指针
     var that = this
+    //构造参数
     var mydata = {
       "year": e.detail.value.year
     };
+    //构造url
     var myurl = app.globalData.url + 'ExternalReviewDocument/addFile?year=' + mydata.year;
+    //选择文件
     wx.chooseMessageFile({
       count: 1,
       type: 'all',
       success: function (res) {
+        //成功处理函数
         console.log("get file success")
         console.log(res)
+        //获得路径
         var mypath = res.tempFiles[0].path
+        //上传文件
         app.wxUploadFile(myurl, mypath, null, (res) => {
+          //成功处理函数
           console.log("upload file success")
           console.log(res)
+          //成功提示
           wx.showToast({
             title: '上传成功',
             icon: '/icons/ok/ok.png',
             duration: 1000,
             success: function () {
+              //延时
               setTimeout(function () {
+                //跳转回查找界面
                 wx.redirectTo({
                   url: '/pages/ExternalReviewManagement/ExternalReviewManagement/ExternalReviewManagementYear/ExternalReviewManagementYear?year=' + that.data.year,
                 })
@@ -48,10 +65,12 @@ Page({
             }
           })
         }, (err) => {
+          //失败处理函数
           console.log(err)
         })
       },
       fail: function (err) {
+        //失败处理函数
         console.log("get file failed")
         console.log(err)
       }
@@ -62,8 +81,10 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (option) {
+    //保存指针
     var that = this;
     console.log(option)
+    //把接收到的数据存储到页面
     that.setData({
       year: option.year
     })
