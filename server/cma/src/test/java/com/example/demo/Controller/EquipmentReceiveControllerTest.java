fd434f37e65b65ca5274426a305cc959b5ea8c04
jammonkeyyy <1538852806@qq.com>
2019-07-07 10:15:52 +0800

    yxp

--- server/cma/src/test/java/com/example/demo/Controller/EquipmentReceiveControllerTest.java
+++ server/cma/src/test/java/com/example/demo/Controller/EquipmentReceiveControllerTest.java
@@ -1,7 +1,46 @@
 package com.example.demo.Controller;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+@Transactional
 public class EquipmentReceiveControllerTest {
 
+        @Autowired
+    EquipmentReceiveController equipmentReceiveController;
+        MockMvc mockMvc;
+        @Before
+        public void setUp() throws Exception {
+            mockMvc = MockMvcBuilders.standaloneSetup(equipmentReceiveController).build();
+        }
+
+    @After
+    public void after() throws Exception {
+    }
+    @Test
+    public void testadd(){
+
+
+    }
+
 }
+
