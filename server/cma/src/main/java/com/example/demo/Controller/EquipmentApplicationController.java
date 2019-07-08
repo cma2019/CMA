2202b4f822aadef5628799f99b3ebaea8574c0f4
jammonkeyyy <1538852806@qq.com>
2019-07-08 15:23:54 +0800

    yxp

--- server/cma/src/main/java/com/example/demo/Controller/EquipmentApplicationController.java
+++ server/cma/src/main/java/com/example/demo/Controller/EquipmentApplicationController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 @Controller
 @RequestMapping(path="/cma/EquipmentApplication")
 public class EquipmentApplicationController {
