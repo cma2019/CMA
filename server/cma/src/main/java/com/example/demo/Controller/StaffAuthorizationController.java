399acd60460af38db10ab6dc88eaddae012482a9
hzhahu <hzh.ahu@qq.com>
2019-07-08 18:08:35 +0800

    hzh

--- server/cma/src/main/java/com/example/demo/Controller/StaffAuthorizationController.java
+++ server/cma/src/main/java/com/example/demo/Controller/StaffAuthorizationController.java
@@ -25,16 +25,19 @@ public class StaffAuthorizationController {
     private StaffManagementRepository staffManagementRepository;
 
     @GetMapping(path = "getAll")
+    //获取所有授权记录
     public @ResponseBody
     JSONObject getAll(){
         JSONObject json=new JSONObject();
         json.put("code",200);
         json.put("msg","成功");
         json.put("data",staffAuthorizationRepository.findAll());
+        //返回所有的授权记录
         return json;
     }
 
     @GetMapping(path = "getOne")
+    //获取某一次授权记录
     public @ResponseBody JSONObject getOne(@RequestParam(value="authorizationId",required = false)long authorizationId){
         JSONObject json=new JSONObject();
         if(staffAuthorizationRepository.existsById(authorizationId)==false){
@@ -42,14 +45,17 @@ public class StaffAuthorizationController {
             json.put("msg","失败,无法找到");
             json.put("data",null);
         }
+        //该次授权记录不存在，返回信息
         else {
             json.put("code",200);
             json.put("msg","成功");
             json.put("data",staffAuthorizationRepository.findById(authorizationId));
+            //返回该次授权记录的详细信息
         }
         return json;
     }
     @PostMapping(path = "addOne")
+    //添加某一次授权记录
     public @ResponseBody JSONObject addOne(@RequestParam(value = "id",required = false) long id,@RequestParam(value = "authorizerId",required = false)long authorizerId,
                                            @RequestParam(value = "content",required = false)String content,@RequestParam(value = "authorizerDate",required = false)String authorizerDate) throws ParseException {
         StaffAuthorization staffAuthorization=new StaffAuthorization();
@@ -59,6 +65,7 @@ public class StaffAuthorizationController {
             json.put("msg","被授权人不存在");
             json.put("data",null);
             return json;
+            //被授权人不在人员列表中，返回信息
         }
         StaffManagement staffManagement=staffManagementRepository.getOne(id);
         if(staffManagementRepository.existsById(authorizerId)==false){
@@ -66,18 +73,29 @@ public class StaffAuthorizationController {
             json.put("msg","授权人不存在");
             json.put("data",null);
             return json;
+            //授权人不在人员列表中，返回信息
         }
         StaffManagement authorizer=staffManagementRepository.getOne(authorizerId);
+        //获取授权人的信息
         staffAuthorization.setId(id);
+        //输入被授权人id
         staffAuthorization.setName(staffManagement.getName());
+        //输入被授权人姓名
         staffAuthorization.setDepartment(staffManagement.getDepartment());
+        //输入被授权人部门
         staffAuthorization.setPosition(staffManagement.getPosition());
+        //输入被授权人职位
         staffAuthorization.setAuthorizerId(authorizerId);
+        //输入授权人id
         staffAuthorization.setAuthorizerName(authorizer.getName());
+        //输入授权人姓名
         staffAuthorization.setContent(content);
+        //输入授权记录内容
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
         staffAuthorization.setAuthorizerDate(sdf.parse(authorizerDate));
+        //输入授权日期
         staffAuthorizationRepository.save(staffAuthorization);
+        //保存授权记录
         json.put("code",200);
         json.put("msg","成功");
         json.put("data",null);
@@ -85,6 +103,7 @@ public class StaffAuthorizationController {
     }
 
     @PostMapping(path = "deleteOne")
+    //删除某次授权记录
     public @ResponseBody JSONObject deleteOne(@RequestParam(value = "authorizationId",required = false)long authorizationId){
         JSONObject json=new JSONObject();
         if(staffAuthorizationRepository.existsById(authorizationId)==false){
@@ -92,8 +111,10 @@ public class StaffAuthorizationController {
             json.put("msg","授权信息不存在");
             json.put("data",null);
         }
+        //该授权记录不存在，返回信息
         else{
             staffAuthorizationRepository.deleteById(authorizationId);
+            //删除该授权记录
             json.put("code",200);
             json.put("msg","成功");
             json.put("data",null);
@@ -102,6 +123,7 @@ public class StaffAuthorizationController {
     }
 
     @PostMapping(path = "modifyOne")
+    //修改授权记录，除了授权记录id其他都是可选项
     public @ResponseBody JSONObject modifyOne(@RequestParam(value = "authorizationId",required = false)long authorizationId,@RequestParam(value = "id",required = false) String id,@RequestParam(value = "authorizerId",required = false)String authorizerId,
                                               @RequestParam(value = "content",required = false)String content,@RequestParam(value = "authorizerDate",required = false)String authorizerDate) throws ParseException {
         JSONObject json=new JSONObject();
@@ -110,10 +132,13 @@ public class StaffAuthorizationController {
             json.put("msg","授权信息不存在");
             json.put("data",null);
         }
+        //该授权记录不存在，返回信息
         else{
             StaffAuthorization staffAuthorization=staffAuthorizationRepository.getOne(authorizationId);
+            //获取该次授权记录的信息
             StaffManagement staffManagement=new StaffManagement();
             if(!id.equals("")) {
+                //判断可选项是否被修改
                 long Id=Long.parseLong(id);
                 if(staffManagementRepository.existsById(Id)==false){
                     json.put("code",210);
@@ -121,27 +146,36 @@ public class StaffAuthorizationController {
                     json.put("data",null);
                     return json;
                 }
+                //修改的被授权人不存在
                 staffManagement=staffManagementRepository.getOne(Id);
+                //修改的被授权人存在
                 staffAuthorization.setId(Id);
                 staffAuthorization.setName(staffManagement.getName());
                 staffAuthorization.setDepartment(staffManagement.getDepartment());
                 staffAuthorization.setPosition(staffManagement.getPosition());
+                //更改授权记录中被授权人的信息
             }
             if(!authorizerId.equals("")) {
+                //判断授权人是否被修改
                 long Id=Long.parseLong(authorizerId);
                 if(staffManagementRepository.existsById(Id)==false){
                     json.put("code",210);
                     json.put("msg","授权人不存在");
                     json.put("data",null);
                     return json;
+                    //修改的授权人不存在
                 }
                 staffManagement=staffManagementRepository.getOne(Id);
+                //获取修改的授权人信息
                 staffAuthorization.setAuthorizerId(Id);
                 staffAuthorization.setAuthorizerName(staffManagement.getName());
+                //修改该记录中授权人的信息
             }
             if(!content.equals(""))
+                //判断授权内容是否被修改
                 staffAuthorization.setContent(content);
             if(!authorizerDate.equals("")){
+                //判断授权日期是否被修改
                 SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
                 staffAuthorization.setAuthorizerDate(sdf.parse(authorizerDate));
             }
@@ -154,11 +188,14 @@ public class StaffAuthorizationController {
     }
 
     @GetMapping(path = "GetAllByStaff")
+    //获取某个人员所有的授权信息
     public @ResponseBody JSONObject getallbystaff(@RequestParam(required = false,value = "id")long id){
+        //输入被授权人id
         JSONObject json=new JSONObject();
         json.put("code",200);
         json.put("msg","成功");
         json.put("data",staffAuthorizationRepository.findAllById(id));
+        //获取所有该人员作为被授权人的授权记录
         return json;
     }
 
--- server/cma/src/main/java/com/example/demo/Controller/TrainingApplicationController.java
+++ server/cma/src/main/java/com/example/demo/Controller/TrainingApplicationController.java
@@ -18,6 +18,7 @@ public class TrainingApplicationController {
     private TrainingApplicationRepository trainingApplicationRepository;
 
     @GetMapping(path = "getAll")
+    //获取所有培训申请
     public @ResponseBody JSONObject getAll(){
         JSONObject json=new JSONObject();
         json.put("code",200);
@@ -27,34 +28,46 @@ public class TrainingApplicationController {
     }
 
     @GetMapping(path = "getOne")
+    //获取一次培训申请信息
     public @ResponseBody JSONObject getOne(@RequestParam(value="id",required = false)long id){
         JSONObject json=new JSONObject();
         if(trainingApplicationRepository.existsById(id)==false){
             json.put("code",210);
             json.put("msg","失败,无法找到");
             json.put("data",null);
+            //不存在该次培训申请
         }
         else {
             json.put("code",200);
             json.put("msg","成功");
             json.put("data",trainingApplicationRepository.findById(id));
+            //获取该次培训申请信息
         }
         return json;
     }
     @PostMapping(path = "addOne")
+    //添加一次培训申请
     public @ResponseBody JSONObject addOne(@RequestParam(value = "name",required = false) String name,@RequestParam(value = "people",required = false)String people,
                                            @RequestParam(value = "trainingUnit",required = false)String trainingUnit,@RequestParam(value = "expense",required = false)long expense,
     @RequestParam(value="reason",required = false)String reason,@RequestParam(value = "department",required = false)String department,@RequestParam(value = "createDate",required = false)String createDate) throws ParseException {
         TrainingApplication trainingApplication=new TrainingApplication();
         trainingApplication.setName(name);
+        //输入培训项目名
         trainingApplication.setPeople(people);
+        //输入培训对象
         trainingApplication.setTrainingUnit(trainingUnit);
+        //输入培训单位
         trainingApplication.setExpense(expense);
+        //输入开销
         trainingApplication.setReason(reason);
+        //输入培训理由
         trainingApplication.setDepartment(department);
+        //输入申请部门
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
         trainingApplication.setCreateDate(sdf.parse(createDate));
+        //输入申请日期
         trainingApplication.setSituation((byte) 0);
+        //初始化为未审核
         JSONObject json=new JSONObject();
         List<TrainingApplication> list=trainingApplicationRepository.findAll();
         for(int i=0;i<list.size();i++){
@@ -66,6 +79,7 @@ public class TrainingApplicationController {
                 return json;
             }
         }
+        //判断该培训申请是否存在
         trainingApplicationRepository.save(trainingApplication);
         json.put("code",200);
         json.put("msg","成功");
@@ -74,15 +88,18 @@ public class TrainingApplicationController {
     }
 
     @PostMapping(path = "deleteOne")
+    //删除某次培训申请
     public @ResponseBody JSONObject deleteOne(@RequestParam(value = "id",required = false)long id){
         JSONObject json=new JSONObject();
         if(trainingApplicationRepository.existsById(id)==false){
             json.put("code",210);
             json.put("msg","无法找到");
             json.put("data",null);
+            //该培训申请不存在
         }
         else{
             trainingApplicationRepository.deleteById(id);
+            //删除该培训申请
             json.put("code",200);
             json.put("msg","成功");
             json.put("data",null);
@@ -91,6 +108,7 @@ public class TrainingApplicationController {
     }
 
     @PostMapping(path = "modifyOne")
+    //修改某次培训申请
     public @ResponseBody JSONObject modifyOne(@RequestParam(value = "id",required = false)long id,@RequestParam(value = "name",required = false) String name,@RequestParam(value = "people",required = false)String people,
                                               @RequestParam(value = "trainingUnit",required = false)String trainingUnit,@RequestParam(value = "expense",required = false)String expense,
                                               @RequestParam(value="reason",required = false)String reason,@RequestParam(value = "department",required = false)String department,@RequestParam(value = "createDate",required = false)String createDate) throws ParseException {
@@ -99,34 +117,45 @@ public class TrainingApplicationController {
             json.put("code",210);
             json.put("msg","无法找到");
             json.put("data",null);
+            //该培训申请不存在
         }
         else{
             TrainingApplication trainingApplication=trainingApplicationRepository.getOne(id);
+            //获取某次培训申请信息
             if(trainingApplication.getSituation()==2)
             {
                 json.put("code",210);
                 json.put("msg","不可修改");
                 json.put("data",null);
                 return json;
+                //申请已通过，不可修改
             }
             if(!name.equals(""))
+                //培训名称是否修改
                 trainingApplication.setName(name);
             if(!people.equals(""))
+                //培训对象是否修改
                 trainingApplication.setPeople(people);
             if(!trainingUnit.equals(""))
+                //培训单位是否修改
                 trainingApplication.setTrainingUnit(trainingUnit);
             if(!expense.equals(""))
+                //培训开销是否修改
                 trainingApplication.setExpense(Integer.parseInt(expense));
             if(!reason.equals(""))
+                //培训理由是否修改
                 trainingApplication.setReason(reason);
             if(!department.equals(""))
+                //申请部门是否修改
                 trainingApplication.setDepartment(department);
             if(!createDate.equals("")){
+                //申请日期是否修改
                 SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
                 trainingApplication.setCreateDate(sdf.parse(createDate));
             }
             if(trainingApplication.getSituation()==1){
                 trainingApplication.setSituation((byte) 0);
+                //若审核未通过，修改后变为未审核状态
                 //trainingApplication.setCreateDate(null);
                 //trainingApplication.setApproveDate(null);
                 //trainingApplication.setApprover(null);
@@ -139,6 +168,7 @@ public class TrainingApplicationController {
         return json;
     }
     @PostMapping("approveOne")
+    //审核一次申请
     public @ResponseBody JSONObject aprroveOne(@RequestParam(value = "id",required = false)long id,@RequestParam(value = "situation",required = false)int situation
             ,@RequestParam(value="approver",required = false)String approver,@RequestParam(value = "approveDate",required = false)String approveDate) throws ParseException {
         JSONObject json=new JSONObject();
@@ -146,20 +176,26 @@ public class TrainingApplicationController {
             json.put("code",210);
             json.put("msg","无法找到");
             json.put("data",null);
+            //该申请不存在
         }
         else{
             TrainingApplication trainingApplication=trainingApplicationRepository.getOne(id);
+            //获取该次申请的信息
             if(trainingApplication.getSituation()==0){
                 if(situation==0){
                     json.put("code",210);
                     json.put("msg","审查错误");
                     json.put("data",null);
                     return json;
+                    //审核结果不可为未审核
                 }
                 trainingApplication.setSituation((byte) situation);
+                //输入审核结果
                 trainingApplication.setApprover(approver);
+                //输入审核人姓名
                 SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
                 trainingApplication.setApproveDate(sdf.parse(approveDate));
+                //输入审核日期
                 trainingApplicationRepository.save(trainingApplication);
                 json.put("code",200);
                 json.put("msg","成功");
@@ -169,6 +205,7 @@ public class TrainingApplicationController {
                 json.put("code",210);
                 json.put("msg","已审查");
                 json.put("data",null);
+                //审核状态不为未审核，则不可审核
             }
         }
         return json;
