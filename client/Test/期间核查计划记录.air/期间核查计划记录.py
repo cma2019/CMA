153224dfd3a6ec3e0017a7d3d7c70911275a724f
zfy <934542721@qq.com>
2019-07-09 16:39:29 +0800

    16.39

---
+++ client/Test/期间核查计划记录.air/期间核查计划记录.py
@@ -0,0 +1,89 @@
+# -*- encoding=utf8 -*-
+__author__ = "pc"
+
+from airtest.core.api import *
+touch(Template(r"tpl1562640685482.png", record_pos=(-0.331, 0.956), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562641186046.png", record_pos=(-0.343, -0.12), resolution=(1080, 2248)))
+
+sleep(2)
+
+touch(Template(r"tpl1562641421095.png", record_pos=(-0.238, 0.044), resolution=(1080, 2248)))
+
+
+touch(Template(r"tpl1562640794907.png", record_pos=(-0.353, -0.122), resolution=(1080, 2248)))
+
+sleep(2)
+
+touch(Template(r"tpl1562641649088.png", record_pos=(0.032, -0.691), resolution=(1080, 2248)))
+
+
+text("kji")
+
+touch(Template(r"tpl1562641481685.png", record_pos=(-0.051, -0.575), resolution=(1080, 2248)))
+text("213")
+
+touch(Template(r"tpl1562641493152.png", record_pos=(-0.087, -0.469), resolution=(1080, 2248)))
+touch(Template(r"tpl1562641504216.png", record_pos=(0.418, 0.354), resolution=(1080, 2248)))
+touch(Template(r"tpl1562641511371.png", record_pos=(-0.114, -0.361), resolution=(1080, 2248)))
+text("ooooo")
+touch(Template(r"tpl1562641529877.png", record_pos=(-0.421, -0.263), resolution=(1080, 2248)))
+
+sleep(2)
+
+touch(Template(r"tpl1562643727012.png", record_pos=(-0.126, -0.02), resolution=(1080, 2248)))
+
+
+
+touch(Template(r"tpl1562643917106.png", record_pos=(0.058, -0.152), resolution=(1080, 2248)))
+
+sleep(2)
+
+
+
+
+
+touch(Template(r"tpl1562643930730.png", record_pos=(-0.074, -0.522), resolution=(1080, 2248)))
+text("123")
+touch(Template(r"tpl1562643950241.png", record_pos=(-0.07, -0.406), resolution=(1080, 2248)))
+touch(Template(r"tpl1562643955749.png", record_pos=(0.419, 0.356), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562643963740.png", record_pos=(-0.034, -0.295), resolution=(1080, 2248)))
+text("3232")
+
+
+touch(Template(r"tpl1562644237990.png", record_pos=(-0.034, -0.178), resolution=(1080, 2248)))
+
+text("432")
+touch(Template(r"tpl1562643980900.png", record_pos=(-0.031, -0.014), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562643955749.png", record_pos=(0.419, 0.356), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562643989174.png", record_pos=(-0.003, 0.156), resolution=(1080, 2248)))
+text("ok")
+touch(Template(r"tpl1562643998574.png", record_pos=(-0.053, 0.275), resolution=(1080, 2248)))
+text("me")
+touch(Template(r"tpl1562644010393.png", record_pos=(-0.03, 0.462), resolution=(1080, 2248)))
+touch(Template(r"tpl1562643955749.png", record_pos=(0.419, 0.356), resolution=(1080, 2248)))
+touch(Template(r"tpl1562644016904.png", record_pos=(-0.112, 0.612), resolution=(1080, 2248)))
+text("ewqeqw")
+
+touch(Template(r"tpl1562644326625.png", record_pos=(-0.423, 0.718), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562652186215.png", record_pos=(-0.106, -0.15), resolution=(1080, 2248)))
+touch(Template(r"tpl1562652195624.png", record_pos=(-0.423, 0.406), resolution=(1080, 2248)))
+
+
+touch(Template(r"tpl1562652222289.png", record_pos=(-0.104, -0.295), resolution=(1080, 2248)))
+text("231")
+
+touch(Template(r"tpl1562652243812.png", record_pos=(-0.421, 0.702), resolution=(1080, 2248)))
+touch(Template(r"tpl1562652264349.png", record_pos=(-0.269, 0.395), resolution=(1080, 2248)))
+
+
+touch(Template(r"tpl1562652186215.png", record_pos=(-0.106, -0.15), resolution=(1080, 2248)))
+touch(Template(r"tpl1562652299415.png", record_pos=(-0.442, -0.897), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562652315079.png", record_pos=(-0.273, -0.143), resolution=(1080, 2248)))
+
+auto_setup(__file__)
---
+++ client/Test/标准管理.air/标准管理.py
@@ -0,0 +1,37 @@
+# -*- encoding=utf8 -*-
+__author__ = "pc"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+touch(Template(r"tpl1562653055880.png", record_pos=(-0.31, 0.954), resolution=(1080, 2248)))
+
+
+touch(Template(r"tpl1562653066432.png", record_pos=(-0.312, 0.555), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562653079913.png", record_pos=(0.399, 0.704), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562653104461.png", record_pos=(-0.348, -0.544), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562653115425.png", record_pos=(-0.408, -0.8), resolution=(1080, 2248)))
+touch(Template(r"tpl1562653122652.png", record_pos=(-0.138, -0.551), resolution=(1080, 2248)))
+touch(Template(r"tpl1562653127247.png", record_pos=(-0.289, -0.734), resolution=(1080, 2248)))
+touch(Template(r"tpl1562653134137.png", record_pos=(0.375, 0.954), resolution=(1080, 2248)))
+touch(Template(r"tpl1562653142754.png", record_pos=(-0.344, -0.488), resolution=(1080, 2248)))
+
+
+
+touch(Template(r"tpl1562653157498.png", record_pos=(-0.106, -0.484), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562653167354.png", record_pos=(-0.419, -0.482), resolution=(1080, 2248)))
+
+
+touch(Template(r"tpl1562653186716.png", record_pos=(-0.41, -0.795), resolution=(1080, 2248)))
+
+touch(Template(r"tpl1562653203622.png", record_pos=(-0.209, -0.545), resolution=(1080, 2248)))
+touch(Template(r"tpl1562653210815.png", record_pos=(-0.291, -0.726), resolution=(1080, 2248)))
+touch(Template(r"tpl1562653218274.png", record_pos=(0.375, 0.969), resolution=(1080, 2248)))
+touch(Template(r"tpl1562653224536.png", record_pos=(-0.272, -0.482), resolution=(1080, 2248)))
+
+
