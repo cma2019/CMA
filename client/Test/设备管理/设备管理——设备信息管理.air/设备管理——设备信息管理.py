c97bd4392d9f8fce1587a681a284cfbfeba5cf21
njucszxy <1027575703@qq.com>
2019-07-09 14:59:11 +0800

    测试

---
+++ client/Test/设备管理/设备管理——设备信息管理.air/设备管理——设备信息管理.py
@@ -0,0 +1,80 @@
+# -*- encoding=utf8 -*-
+__author__ = "njucszxy"
+
+from airtest.core.api import *
+ 
+auto_setup(__file__)
+
+
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco = AndroidUiautomationPoco()
+
+touch(Template(r"tpl1562643606699.png", record_pos=(-0.322, 0.806), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643616581.png", record_pos=(0.001, 0.297), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643625858.png", record_pos=(0.012, 0.433), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643639549.png", record_pos=(-0.364, 0.295), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643648506.png", record_pos=(-0.037, -0.658), resolution=(1440, 2560)))
+
+text("Mac")
+
+touch(Template(r"tpl1562643661533.png", record_pos=(-0.086, -0.557), resolution=(1440, 2560)))
+
+text("Macbook 15")
+
+touch(Template(r"tpl1562643683093.png", record_pos=(-0.108, -0.439), resolution=(1440, 2560)))
+
+text("Intel Core I7")
+
+touch(Template(r"tpl1562643702989.png", record_pos=(-0.095, -0.335), resolution=(1440, 2560)))
+
+text("16G")
+
+touch(Template(r"tpl1562643715297.png", record_pos=(-0.112, -0.239), resolution=(1440, 2560)))
+
+text("256G")
+
+touch(Template(r"tpl1562643736641.png", record_pos=(-0.074, -0.126), resolution=(1440, 2560)))
+
+text("47385736")
+
+touch(Template(r"tpl1562643837638.png", record_pos=(-0.133, -0.023), resolution=(1440, 2560)))
+
+text("教学")
+
+
+touch(Template(r"tpl1562643754146.png", record_pos=(0.432, 0.094), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643759962.png", record_pos=(-0.422, 0.183), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643867910.png", record_pos=(-0.01, 0.381), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643877502.png", record_pos=(-0.422, -0.108), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643892392.png", record_pos=(-0.095, -0.235), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643910787.png", record_pos=(0.442, -0.242), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643921591.png", record_pos=(-0.115, -0.233), resolution=(1440, 2560)))
+
+text("512G")
+
+touch(Template(r"tpl1562643945364.png", record_pos=(-0.424, 0.183), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643963561.png", record_pos=(-0.003, 0.385), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562643975011.png", record_pos=(-0.279, -0.105), resolution=(1440, 2560)))
+
+
+
+
+
+
+
+
+
+
+
---
+++ client/Test/质量体系管理.air/质量体系——质量体系.py
@@ -0,0 +1,61 @@
+# -*- encoding=utf8 -*-
+__author__ = "njucszxy"
+
+from airtest.core.api import *
+ 
+auto_setup(__file__)
+
+
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco = AndroidUiautomationPoco()
+
+touch(Template(r"tpl1562653642218.png", record_pos=(-0.321, 0.808), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653657057.png", record_pos=(0.003, -0.556), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653673439.png", record_pos=(0.016, -0.415), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653691075.png", record_pos=(-0.388, 0.382), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653706221.png", record_pos=(0.426, -0.653), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653711751.png", record_pos=(0.432, -0.532), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653720854.png", record_pos=(-0.113, -0.418), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653730050.png", record_pos=(-0.131, -0.308), resolution=(1440, 2560)))
+
+text("诸葛亮")
+
+touch(Template(r"tpl1562653742348.png", record_pos=(-0.119, -0.211), resolution=(1440, 2560)))
+
+text("知天易逆天难")
+
+touch(Template(r"tpl1562653865305.png", record_pos=(-0.422, -0.117), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653873399.png", record_pos=(0.003, -0.422), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653881459.png", record_pos=(-0.288, -0.61), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653889412.png", record_pos=(0.377, 0.799), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653902984.png", record_pos=(0.0, -0.507), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653915385.png", record_pos=(-0.267, -0.16), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653932090.png", record_pos=(-0.125, -0.167), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653942495.png", record_pos=(-0.444, -0.765), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653952078.png", record_pos=(0.013, -0.3), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653961017.png", record_pos=(-0.006, 0.137), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653975459.png", record_pos=(-0.278, -0.163), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653984542.png", record_pos=(0.015, -0.057), resolution=(1440, 2560)))
+
+touch(Template(r"tpl1562653992022.png", record_pos=(-0.422, -0.162), resolution=(1440, 2560)))
+
+
+
